44a2cd29bfc87eccca2dc5d1f64c60ee077315e2
joehattori <joe4.ninjahattori@icloud.com>
2019-02-09 15:24:59 +0900

    Initial Commit

---
+++ image-cropping-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  image-cropping-ios
+//
+//  Created by 服部穣 on 2019/02/09.
+//  Copyright © 2019 服部穣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
